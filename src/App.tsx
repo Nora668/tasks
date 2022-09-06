c257b6ab10d734b9b40a87ee95b81e12b250b7d3
Nora668 <norapan668@gmail.com>
2022-09-06 17:45:56 -0400

    Task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
+            <header className="App-header" style={{ background: "#1B41DC" }}>
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <header>New Header</header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Editor: Yuqing Pan Hello World
             </p>
+            <img
+                src="https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2018/11/what-is-html-3.jpg"
+                alt="Just some random image"
+            />
+            <ul>
+                <li>Element 1</li>
+                <li>Element 2</li>
+                <li>Element 3</li>
+            </ul>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <div className="App-rect">
+                <Container>
+                    <Row>
+                        <Col>First column</Col>
+                        <br></br>
+                        <Col>Second column</Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 }
